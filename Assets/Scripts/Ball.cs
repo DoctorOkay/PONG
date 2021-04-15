7b9f53d56f5abc86c13efa766d50169123a57290
Dylan Vidas <viddylan@gmail.com>
2021-04-15 08:43:26 -0500

    Bouncing a ball some more, knocking some rust off

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,32 +5,53 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] float ballSpeed = 10f;
-    [SerializeField] Paddle playerPaddle;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // just move it to the left to start
+        // step 1: move the ball
+        Vector2 ballVel = new Vector2(ballSpeed, ballSpeed / 2);
         Vector2 newBallPos = new Vector2();
 
-        float scaledBallSpeed = ballSpeed * Time.deltaTime;
+        newBallPos = transform.position;
+        newBallPos += ballVel;
 
-        newBallPos.x = transform.position.x - scaledBallSpeed;
-        newBallPos.y = transform.position.y;
+        // set up the min and max points of the screen
+        Vector3 screenMin = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        Vector3 screenMax = new Vector3(Screen.width, Screen.height, 0);
+        screenMax = Camera.main.ScreenToWorldPoint(screenMax);
 
-        /* for collision all of these need to be true
-         * ballLeft > paddleRight
-         * ballRight < paddleLeft
-         * ballTop > paddleBottom
-         * ballBottom < paddleTop
-         */
-
+        // step 2: bounce ball around the screen
+        if (newBallPos.x < screenMin.x)
+        {
+            // place the ball on the screen edge and reverse the velocity
+            newBallPos.x = screenMin.x + GetComponent<SpriteRenderer>().bounds.size.x;
+            ballVel.x = -ballVel.x;
+        }
+        if (newBallPos.x > screenMax.x)
+        {
+            // place the ball on the screen edge and reverse the velocity
+            newBallPos.x = screenMax.x - GetComponent<SpriteRenderer>().bounds.size.x;
+            ballVel.x = -ballVel.x;
+        }
+        if (newBallPos.y < screenMin.y)
+        {
+            // place the ball on the screen edge and reverse the velocity
+            newBallPos.y = screenMin.y + GetComponent<SpriteRenderer>().bounds.size.y;
+            ballVel.y = -ballVel.y;
+        }
+        if (newBallPos.y > screenMax.y)
+        {
+            // place the ball on the screen edge and reverse the velocity
+            newBallPos.y = screenMax.y - GetComponent<SpriteRenderer>().bounds.size.y;
+            ballVel.y = -ballVel.y;
+        }
 
         transform.position = newBallPos;
     }
